0e1294a11f2f039e2f85139218eed08f2ec19e8e
Egraf99 <egorkhodin@gmail.com>
2021-10-04 16:06:49 +0300

    добавление виджета кнопки добавления матчей и другого на экран игр

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.stacklayout import StackLayout
-from kivy.uix.label import Label
-from kivy.uix.button import Button
-from kivy.uix.scrollview import ScrollView
-from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ObjectProperty
 from kivy.metrics import dp
 
@@ -23,24 +17,13 @@ class GameScreen(BoxLayout):
         super().__init__(**kwargs)
         self.main_page()
 
-    def check_menu_button_click(self, button):
-        for bn in self.buttons:
-            if button is bn:
-                bn.disabled = True
-            else:
-                bn.disabled = False
-
     def main_page(self):
-        # self.check_menu_button_click(button)
-
         self.games_layout.clear_widgets()
 
-        # self.top_bn.text = button.text
-
-        my_games = self.take_games()
+        my_games = self.take_games
 
         game_table = MDDataTable(
-            use_pagination=True,
+            use_pagination=False,
             check=True,
             column_data=[
                 ('Дата', dp(30)),
@@ -51,21 +34,11 @@ class GameScreen(BoxLayout):
                 ('Фамилия', dp(30))
             ],
             row_data=my_games
-            )
+        )
 
         self.games_layout.add_widget(game_table)
 
-    def int_to_date(self):
-        pass
-
-    def from_db_to_list(self) -> list:
-        pass
-
-    def change_label(self, button, text):
-        self.check_menu_button_click(button)
-
-        self.label.text = text
-
+    @property
     def take_games(self):
         db = ConnDB()
         games = db.take_games()
@@ -76,6 +49,9 @@ class GameScreen(BoxLayout):
             league, date, stadium, team_home, team_guest, referee = game_info[:6]
             list_of_games.append([date, league, stadium, team_home, team_guest, referee])
         return list_of_games
+
+    def callback(self, instance):
+        print(instance.icon)
 
 
 class MainApp(MDApp):
