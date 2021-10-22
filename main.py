266b10ddb1675dc53074d860f4385e155ec3a8b0
Egraf99 <77966173+Egraf99@users.noreply.github.com>
2021-10-22 23:28:53 +0300

    переработка функции take_id класса DB, добавление do_backspase в поля ввода даты и телефона, упрощение функций main.py

--- main.py
+++ main.py
@@ -38,12 +38,14 @@ class GameScreen(BoxLayout):
 
         self.add_game_dialog = ObjectProperty()
 
+        self.table_games = MatchTable()
+
         # при первом включении необходимо показать контент
         self._create_main_page()
 
     def _create_main_page(self):
         self.games_layout.clear_widgets()
-        self.games_layout.add_widget(MatchTable())
+        self.games_layout.add_widget(self.table_games)
 
     def add_button_callback(self, instance):
         """Вызывается при нажатии на одну из кнопок MDFloatingActionButtonSpeedDial.
@@ -55,7 +57,7 @@ class GameScreen(BoxLayout):
 
     def pop_dialog_add_match(self):
         """Открывает Dialog для добавления новой игры."""
-        self.add_game_dialog = AddDataWindow(type_="game")
+        self.add_game_dialog = AddDataWindow(type_="games")
         self.add_game_dialog.open()
 
     def update_db(self, event):
@@ -82,7 +84,7 @@ class AddDataWindow(MDDialog):
         super(AddDataWindow, self).__init__(**kwargs)
 
     def _set_content_cls(self, type_, filled):
-        if type_ == "game":
+        if type_ == "games":
             self.content_cls = AddGameContent(filled, caller=self.caller_)
         if type_ == "referee":
             self.content_cls = AddRefereeContent(filled, caller=self.caller_)
@@ -111,8 +113,10 @@ class MatchTable(MDDataTable):
     """Класс таблицы для игр."""
 
     def __init__(self, **kwargs):
+        self.elevation = 100
+        self.rows_num = 10
         self.cell_size = dp(25)
-        self.use_pagination = False
+        self.use_pagination = True
         self.check = True
         self.column_data = [('Дата', dp(28)),
                             ('Время', dp(12)),
@@ -121,28 +125,30 @@ class MatchTable(MDDataTable):
                             ('Хозяева', self.cell_size),
                             ('Гости', self.cell_size)]
 
-        self.row_data = self._take_games
+        self.row_data = self._take_games()
         super(MatchTable, self).__init__(**kwargs)
 
-    @property
+    def update(self):
+        """Обновляет таблицу"""
+        self.row_data = self._take_games()
+
     def _take_games(self) -> list:
         """Возвращает преобразованные в табличные значения данные из БД."""
         games = DB.take_games()
-
         list_of_games = []
         for game_info in games:
-            league, date, time, stadium, team_home, team_guest = game_info[:6]
+            league, date, time_, stadium, team_home, team_guest = game_info[:6]
 
             date = self._date_list_in_str(date.split())
-            time = self._time_int_in_str(time)
+            time_ = self._time_int_in_str(time_)
 
-            list_of_games.append([date, time, league, stadium, team_home, team_guest])
+            list_of_games.append([date, time_, league, stadium, team_home, team_guest])
 
         return list_of_games
 
-    def _time_int_in_str(self, time: int) -> str:
+    def _time_int_in_str(self, time_: int) -> str:
         """Преобразует значние времени взятое из базы данных в виде 4-х целых чисел в формат HH:MM."""
-        hour, minute = time // 100, time % 100
+        hour, minute = time_ // 100, time_ % 100
 
         hour = self._change_if_less_ten(hour)
         minute = self._change_if_less_ten(minute)
@@ -255,46 +261,53 @@ class AddDataContent(RecycleView):
         caller_field_text = ""
         data = {}
 
-        for field in fields:
+        for field in fields[::-1]:
             if field.add_text_in_parent:
                 # запоминаем текст из полей, данные из которых будут записаны в вызывающем поле
-                caller_field_text += field.text
+                caller_field_text = " ".join([caller_field_text, field.text])
 
             if field.is_notnull and not field.text:  # ищем необходимые не заполненные поля
                 not_fill_fields.append(field.name.capitalize())
 
             elif field.text and field.set_id:  # поле, имеющее всплывающее окно записываются в БД через id
-                if field.data_table == 'referee':
-                    # в таблице referee нет отдельного поля "name", как в других таблицах, поэтому делаем отдельный
-                    # запрос с именем и фамилией
-                    name = field.text.split(' ')
-                    if name[0]:  # введено ли значение в поле
-                        second_name, first_name = name[:2]
-                        data[field.data_key] = DB.take_id_from_referee(field.data_table, first_name, second_name)[0]
+                # если полей заполнения несколько, разбиваем строку по пробелу
+                all_conditions = field.text.split(' ') if len(field.what_fields_child_fill) > 1 else [field.text]
+
+                conditions_dict = {}
+
+                # составляем псписок, где ключ - поле в БД, по которому искать, значение - фильтрующее значение
+                for inx, condition in enumerate(all_conditions):
+                    try:
+                        conditions_dict[field.what_fields_child_fill[inx]] = condition
+                    except IndexError:
+                        continue
+
+                # запрашиваем id исходя из условий
+                id_ = DB.take_id(field.data_table, conditions_dict)[0]
+                if id_:
+                    data[field.data_key] = id_
                 else:
-                    id_ = DB.take_id(field.data_table, field.text)[0]
-                    if id_:
-                        data[field.data_key] = DB.take_id(field.data_table, field.text)[0]
-                    else:
-                        print(f'Для имени {field.text} нет id')
+                    print(f'Для имени {field.text} нет id')
 
             elif field.text:  # поля не пустые, текст из которых прямо идет в БД
                 data[field.data_key] = field.text
 
-        if not_fill_fields:
+        if not_fill_fields:  # если есть незаполненные поля вызываем подсказку и возвращаем неуспех
             not_filled = ", ".join(not_fill_fields)
             open_dialog(f'The {not_filled} field/s is not filled on')
             return False
 
         if self.caller_:
-            self.caller_.text = caller_field_text
+            self.caller_.text = caller_field_text.strip()
 
         DB.insert(self.data_table, data)
 
         # self.parent - BoxLayout, self.parent.parent - MDCard, self.parent.parent.parent - AddDataWindow
         self.parent.parent.parent.dismiss()
 
-        open_dialog("Successfully added")
+        if not self.caller_:
+            open_dialog("Successfully added")
+            main.game_screen.table_games.update()
 
         return True
 
@@ -312,28 +325,28 @@ class AddDataContent(RecycleView):
 
 class AddGameContent(AddDataContent):
     def __init__(self, filled, caller=None, **kwargs):
-        self.data_table = "game"
+        self.data_table = "games"
         self.items = [
-            {'name': 'Stadium', 'type': 'textfield', 'what_field_child_fill': ['name'],
+            {'name': 'Stadium', 'type': 'textfield', 'what_fields_child_fill': ['name'],
              'data_table': 'stadium', 'data_key': 'stadium_id', 'drop_menu': True, 'notnull': True},
             {'name': 'Data and Time', 'type': 'textfield', 'data_key': 'date_and_time', 'notnull': True},
-            {'name': 'League', 'type': 'textfield', 'what_field_child_fill': ['name'],
+            {'name': 'League', 'type': 'textfield', 'what_fields_child_fill': ['name'],
              'data_table': 'league', 'data_key': 'league_id', 'drop_menu': True, 'notnull': True},
-            {'name': 'Home team', 'type': 'textfield', 'what_field_child_fill': ['name'],
+            {'name': 'Home team', 'type': 'textfield', 'what_fields_child_fill': ['name'],
              'data_table': 'team', 'data_key': 'team_home', 'drop_menu': True, 'notnull': True},
-            {'name': 'Guest team', 'type': 'textfield', 'what_field_child_fill': ['name'],
+            {'name': 'Guest team', 'type': 'textfield', 'what_fields_child_fill': ['name'],
              'data_table': 'team', 'data_key': 'team_guest', 'drop_menu': True, 'notnull': True},
             {'name': 'Chief referee', 'type': 'textfield',
-             'what_field_child_fill': ['second_name', 'first_name', 'third_name'],
+             'what_fields_child_fill': ['second_name', 'first_name', 'third_name'],
              'data_table': 'referee', 'data_key': 'referee_chief', 'drop_menu': True, 'notnull': True},
             {'name': 'First referee', 'type': 'textfield',
-             'what_field_child_fill': ['second_name', 'first_name', 'third_name'],
+             'what_fields_child_fill': ['second_name', 'first_name', 'third_name'],
              'data_table': 'referee', 'data_key': 'referee_first', 'drop_menu': True},
             {'name': 'Second referee', 'type': 'textfield',
-             'what_field_child_fill': ['second_name', 'first_name', 'third_name'],
+             'what_fields_child_fill': ['second_name', 'first_name', 'third_name'],
              'data_table': 'referee', 'data_key': 'referee_second', 'drop_menu': True},
             {'name': 'Reserve referee', 'type': 'textfield',
-             'what_field_child_fill': ['second_name', 'first_name', 'third_name'],
+             'what_fields_child_fill': ['second_name', 'first_name', 'third_name'],
              'data_table': 'referee', 'data_key': 'referee_reserve', 'drop_menu': True}
         ]
 
@@ -350,7 +363,7 @@ class AddRefereeContent(AddDataContent):
              'add_text_in_parent': True},
             {'name': 'Third name', 'type': 'textfield', 'data_key': 'third_name'},
             {'name': 'Phone', 'type': 'textfield', 'data_key': 'phone'},
-            {'name': 'Category', 'type': 'textfield', 'what_field_child_fill': ['name'],
+            {'name': 'Category', 'type': 'textfield', 'what_fields_child_fill': ['name'],
              'data_table': 'category', 'data_key': 'category_id', 'drop_menu': True}
         ]
 
@@ -362,7 +375,7 @@ class AddStadiumContent(AddDataContent):
         self.data_table = "stadium"
         self.items = [
             {'name': 'Name', 'type': 'textfield', 'data_key': 'name', 'notnull': True, 'add_text_in_parent': True},
-            {'name': 'City', 'type': 'textfield', 'what_field_child_fill': ['name'],
+            {'name': 'City', 'type': 'textfield', 'what_fields_child_fill': ['name'],
              'data_table': 'city', 'data_key': 'city_id', 'drop_menu': True, 'notnull': True},
             {'name': 'Address', 'type': 'textfield', 'data_key': 'address', 'notnull': True},
         ]
@@ -415,7 +428,7 @@ class TextField(MDTextField):
         self.data_key = instr.setdefault('data_key')
         self.data_table = instr.setdefault('data_table')
         self.is_notnull = instr.setdefault('notnull', False)
-        self.what_field_child_fill = instr.setdefault('what_field_child_fill')
+        self.what_fields_child_fill = instr.setdefault('what_fields_child_fill')
         self.add_text_in_parent = instr.setdefault('add_text_in_parent', False)
 
         self.set_id = False
@@ -477,13 +490,27 @@ class PhoneTF(TFWithoutDrop):
             substring = ''
         return super(PhoneTF, self).insert_text(substring, from_undo=from_undo)
 
+    def do_backspace(self, from_undo=False, mode='bkspc'):
+        cursor = self.cursor_col
+
+        if self.text.startswith("+"):
+            cursor -= 1
+
+        doble_bkspc = False
+        if cursor in [3, 7, 11, 14]:
+            doble_bkspc = True
+
+        if doble_bkspc:
+            super(PhoneTF, self).do_backspace(from_undo=from_undo, mode='bkspc')
+        super(PhoneTF, self).do_backspace(from_undo=from_undo, mode='bkspc')
+
     def on_text_validate(self):
         pat = "^(\+7|8)\([0-9]{3}\)[0-9]{3}(-[0-9]{2}){2}$"
 
         if not re.match(pat, self.text):
             # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             # делаем красным очищаем и не меняем фокус
-            self.text = "incorrect phone"
+            self.text = ""
 
         super(PhoneTF, self).on_text_validate()
 
@@ -508,13 +535,23 @@ class DateAndTimeTF(TFWithoutDrop):
             substring = ''
         return super(DateAndTimeTF, self).insert_text(substring, from_undo=from_undo)
 
+    def do_backspace(self, from_undo=False, mode='bkspc'):
+        cursor = self.cursor_col
+        doble_bkspc = False
+        if cursor in [4, 7, 12, 15, 18]:
+            doble_bkspc = True
+
+        if doble_bkspc:
+            super(DateAndTimeTF, self).do_backspace(from_undo=from_undo, mode='bkspc')
+        super(DateAndTimeTF, self).do_backspace(from_undo=from_undo, mode='bkspc')
+
     def on_text_validate(self):
         pat = "^(0?[1-9]|[12][0-9]|3[01]).(0?[0-9]|1[012]).(19|20)?[0-9]{2} ([01][1-9]|2[0-4]):[0-6][0-9]$"
 
         if not re.match(pat, self.text):
             # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
             # делаем красным очищаем и не меняем фокус
-            self.text = "incorrect data or time"
+            self.text = ""
 
         super(DateAndTimeTF, self).on_text_validate()
 
@@ -530,6 +567,7 @@ class TFWithDrop(TextField):
     def add_item_in_text_input(self, text_item):
         self.text = text_item
         self.drop_menu.dismiss()
+        self.change_focus = True
         self.set_next_focus()
 
     def on_focus_(self):
@@ -593,11 +631,11 @@ class TFWithDrop(TextField):
     def _make_dict_filled_field_in_children(self) -> dict:
         filled_fields = {}
 
-        if len(self.what_field_child_fill) == 1:
-            filled_fields.update({self.what_field_child_fill[0]: self.text})
-        elif len(self.what_field_child_fill) > 1:
+        if len(self.what_fields_child_fill) == 1:
+            filled_fields.update({self.what_fields_child_fill[0]: self.text})
+        elif len(self.what_fields_child_fill) > 1:
             text_fields = self.text.split()
-            for inx, field in enumerate(self.what_field_child_fill):
+            for inx, field in enumerate(self.what_fields_child_fill):
                 try:
                     filled_fields.update({field: text_fields[inx]})
                 except IndexError:
@@ -620,7 +658,9 @@ class MainApp(MDApp):
         self.theme_cls.primary_palette = "Gray"
         self.theme_cls.theme_style = "Dark"
 
-        return GameScreen()
+        self.game_screen = GameScreen()
+
+        return self.game_screen
 
 
 if __name__ == "__main__":
